fd425a75e287c60a2e12c20c8aa65e2946377a77
cranefirst <3443038221@qq.com>
2026-02-05 16:36:44 +0800

    3-2

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -96,6 +96,12 @@ ssize_t sys_user_yield() {
   schedule();
   return 0;
 }
+ssize_t sys_user_printpa(uint64 va)
+{
+  uint64 pa = (uint64)user_va_to_pa((pagetable_t)(current->pagetable), (void*)va);
+  sprint("%lx\n", pa);
+  return 0;
+}
 
 //
 // [a0]: the syscall number; [a1] ... [a7]: arguments to the syscalls.
@@ -116,6 +122,8 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_fork();
     case SYS_user_yield:
       return sys_user_yield();
+    case SYS_user_printpa:
+      return sys_user_printpa(a1);
     default:
       panic("Unknown syscall %ld \n", a0);
   }
